4f5e1bd795dec79817d7a4865d2829f3cd716c8b
Jared <translucency@outlook.com>
2021-03-04 07:43:50 -0800

    Add project files.

---
+++ ConcurrentList.Tests/ConcurrentListTests.cs
@@ -0,0 +1,161 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentList.Tests
+{
+    [TestClass]
+    public class ConcurrentListTests
+    {
+        private readonly int _startCount = 500_000;
+        private ConcurrentList<int> _list;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _list = new ConcurrentList<int>();
+            for (var i = 0; i < _startCount; i++)
+            {
+                _list.Add(i);
+            }
+        }
+
+
+        [TestMethod]
+        public void IListTests()
+        {
+            Assert.IsTrue(_list.Contains(500));
+            Assert.IsTrue(_list.Contains(100_002));
+
+            var reset1 = new ManualResetEvent(false);
+            var reset2 = new ManualResetEvent(false);
+
+            // Add and remove items from two separate background threads.
+            _ = Task.Run(() =>
+            {
+                for (var i = 0; i < 5_000; i++)
+                {
+                    Assert.IsTrue(_list.Remove(500 + i));
+                    _list.RemoveAt(100_000);
+                    _list.Add(42);
+                    _list.Insert(200_000, 100);
+                }
+                reset1.Set();
+            });
+
+            _ = Task.Run(() =>
+            {
+                for (var i = 5_000; i < 10_000; i++)
+                {
+                    Assert.IsTrue(_list.Remove(500 + i));
+                    _list.RemoveAt(100_000);
+                    _list.Add(42);
+                    _list.Insert(200_000, 100);
+                }
+                reset2.Set();
+            });
+
+            reset1.WaitOne();
+            reset2.WaitOne();
+
+            Assert.IsFalse(_list.Contains(500));
+            Assert.IsFalse(_list.Contains(100_002));
+            // We should have the original count with which we started.
+            Assert.AreEqual(_startCount, _list.Count);
+        }
+
+        [TestMethod]
+        public void ForAsync_GivenMemberReentrancy_Throws()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                _list.ForAsync(i =>
+                {
+                    _list.Add(3);
+                }).GetAwaiter().GetResult();
+            });
+        }
+
+        [TestMethod]
+        public void ForEachAsync_GivenMemberReentrancy_Throws()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                _list.ForEachAsync(item =>
+                {
+                    _list.Remove(item);
+                }).GetAwaiter().GetResult();
+            });
+        }
+
+
+        [TestMethod]
+        public void ForAsync_GivenCollectionModifiedInAnotherThread_WaitsAndSucceeds()
+        {
+            var reset = new ManualResetEvent(false);
+            var loopStart = new ManualResetEvent(false);
+            var lastItem = _list.Last();
+            var lastItemFound = false;
+
+            _ = Task.Run(async () =>
+            {
+                await _list.ForAsync(i =>
+                {
+                    loopStart.Set();
+                    if (i == lastItem)
+                    {
+                        lastItemFound = true;
+                    }
+                });
+                reset.Set();
+            });
+
+            loopStart.WaitOne();
+
+            Assert.IsTrue(_list.Remove(lastItem));
+
+            // The last item was found, so we know the remove operation
+            // waited until the loop was finished.
+            Assert.IsTrue(lastItemFound);
+
+            reset.WaitOne();
+        }
+
+        [TestMethod]
+        public void ForEachAsync_GivenCollectionModifiedInAnotherThread_WaitsAndSucceeds()
+        {
+            var reset = new ManualResetEvent(false);
+            var loopStart = new ManualResetEvent(false);
+            var lastItem = _list.Last();
+            var lastItemFound = false;
+
+            _ = Task.Run(async () =>
+            {
+                await _list.ForEachAsync(i =>
+                {
+                    loopStart.Set();
+                    if (i == lastItem)
+                    {
+                        lastItemFound = true;
+                    }
+                });
+                reset.Set();
+            });
+
+            loopStart.WaitOne();
+            
+            Assert.IsTrue(_list.Remove(lastItem));
+
+            // The last item was found, so we know the remove operation
+            // waited until the loop was finished.
+            Assert.IsTrue(lastItemFound);
+
+            reset.WaitOne();
+        }
+
+    }
+}
---
+++ ConcurrentList/ConcurrentList.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Concurrent
+{
+    /// <summary>
+    /// A simple, lock-based implementation of a thread-safe List<T>.
+    /// Note that it will still throw if the collection is modified during
+    /// enumeration.  Instead, use the ForAsync and ForEach async methods,
+    /// which are thread-safe.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ConcurrentList<T> : IList<T>
+    {
+        private readonly ConcurrentDictionary<int, string> _forThreads = new ConcurrentDictionary<int, string>();
+        private readonly List<T> _list = new List<T>();
+        private readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
+        public int Count
+        {
+            get
+            {
+                CheckForLoopThreads();
+                try
+                {
+                    _lock.Wait();
+                    return _list.Count;
+                }
+                finally
+                {
+                    _lock.Release();
+                }
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                CheckForLoopThreads();
+                try
+                {
+                    _lock.Wait();
+                    return _list[index];
+                }
+                finally
+                {
+                    _lock.Release();
+                }
+            }
+            set
+            {
+                CheckForLoopThreads();
+                try
+                {
+                    _lock.Wait();
+                    _list[index] = value;
+                }
+                finally
+                {
+                    _lock.Release();
+                }
+            }
+        }
+        public void Add(T item)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                _list.Add(item);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public void Clear()
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                _list.Clear();
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                return _list.Contains(item);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                _list.CopyTo(array, arrayIndex);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        /// <summary>
+        /// Perform an action for each index in the List.  You cannot access other members of
+        /// this ConcurrentList from within the supplied action.
+        /// </summary>
+        /// <param name="iterateAction">An Action that takes the current index as a parameter.</param>
+        /// <returns>Awaitable task.</returns>
+        public async Task ForAsync(Action<int> action)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                await _lock.WaitAsync();
+
+                _forThreads.TryAdd(Thread.CurrentThread.ManagedThreadId, string.Empty);
+
+                for (var i = 0; i < _list.Count; i++)
+                {
+                    action.Invoke(i);
+                }
+            }
+            finally
+            {
+                _forThreads.TryRemove(Thread.CurrentThread.ManagedThreadId, out _);
+                _lock.Release();
+            }
+        }
+
+        /// <summary>
+        /// Perform an action for each item in the List.  You cannot access other members of
+        /// this ConcurrentList from within the supplied action.
+        /// </summary>
+        /// <param name="action">An action that takes the current List item as a parameter.</param>
+        /// <returns></returns>
+        public async Task ForEachAsync(Action<T> action)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                await _lock.WaitAsync();
+
+                _forThreads.TryAdd(Thread.CurrentThread.ManagedThreadId, string.Empty);
+
+                foreach (var item in _list)
+                {
+                    action.Invoke(item);
+                }
+            }
+            finally
+            {
+                _forThreads.TryRemove(Thread.CurrentThread.ManagedThreadId, out _);
+                _lock.Release();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            CheckForLoopThreads();
+
+            try
+            {
+                _lock.Wait();
+                return _list.GetEnumerator();
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                return _list.GetEnumerator();
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                return _list.IndexOf(item);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                _list.Insert(index, item);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                return _list.Remove(item);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            CheckForLoopThreads();
+            try
+            {
+                _lock.Wait();
+                _list.RemoveAt(index);
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        private void CheckForLoopThreads()
+        {
+            if (_forThreads.ContainsKey(Thread.CurrentThread.ManagedThreadId))
+            {
+                throw new InvalidOperationException("Unable to access ConcurrentList members from within a " +
+                    $"{nameof(ForAsync)} or {nameof(ForEachAsync)} loop.");
+            }
+        }
+    }
+}
