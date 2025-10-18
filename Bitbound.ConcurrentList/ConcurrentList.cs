4a6b76d1eade627a80814a5cb5c59e965bb06f25
Jared Goodwin <translucency@outlook.com>
2025-10-17 18:34:14 -0700

    Update project files for .NET 9.0 compatibility and adjust package references

--- Bitbound.ConcurrentList/ConcurrentList.cs
+++ Bitbound.ConcurrentList/ConcurrentList.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Bitbound.ConcurrentList
 {
